1b9ee53a7bd3f4c11260b8a3be0a9b8602212584
Laurent Ghirardotti <laurent@ghirardotti.fr>
2023-05-05 11:26:41 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,31 @@
+module exemple/parse
+
+go 1.20
+
+require github.com/gocolly/colly v1.2.0
+
+require (
+	github.com/JohannesKaufmann/html-to-markdown v1.4.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/mysql v1.5.0 // indirect
+	gorm.io/gorm v1.25.0 // indirect
+)
---
+++ vatican_parser.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "github.com/gocolly/colly"
+    "strings"
+    "path"
+    "path/filepath"
+    "regexp"
+    "time"
+    md "github.com/JohannesKaufmann/html-to-markdown"
+    "github.com/JohannesKaufmann/html-to-markdown/plugin"
+)
+
+func main() {
+    c := colly.NewCollector(
+        colly.AllowedDomains("vatican.va", "www.vatican.va"),
+    )
+    opt := &md.Options{
+        CodeBlockStyle: "fenced", // default: indented
+        EmDelimiter: "*", // default: _
+    }
+    converter := md.NewConverter("", true, opt)
+    converter.Use(plugin.GitHubFlavored())
+
+    c.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting", r.URL)
+    })
+
+    c.OnError(func(r *colly.Response, err error) {
+        fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+    })
+
+    // Parcourir tous les types de documents
+    c.OnHTML("#accordionmenu > ul > li", func(e *colly.HTMLElement) {
+        //fmt.Println("Doc type:", e.ChildText("a[0]"))
+        e.Request.Visit(e.ChildAttr("a", "href"))
+
+        // Parcourir les sous sections (années et mois) d'un type de document
+        e.ForEach("ul li a", func(_ int, el *colly.HTMLElement) {
+            //fmt.Println("subDoc type:", strings.TrimSpace(e.Text))
+            el.Request.Visit(el.Attr("href"))
+        })
+    })
+
+    // Parcourir les documents d'un type donné
+    c.OnHTML(".vaticanindex h1 a", func(e *colly.HTMLElement) {
+        e.Request.Visit(e.Attr("href"))
+    })
+
+    // c.OnHTML(".breadcrumb .breadcrumbs", func(e *colly.HTMLElement) {
+    //     path := ""
+    //     e.ForEach("a", func(_ int, el *colly.HTMLElement) {
+    //         path = filepath.Join(path, strings.TrimSpace(e.Text))
+    //     })
+    //     fmt.Println("Breadcrumbs:", path)
+    // })
+
+    c.OnHTML(".documento .testo", func(e *colly.HTMLElement) {
+        docUrl          := e.Request.URL.String()
+        docBaseUrlExt   := path.Base(docUrl)
+        docBaseUrl      := strings.TrimSuffix(docBaseUrlExt, filepath.Ext(docBaseUrlExt))
+        re              := regexp.MustCompile(`[0-9]{8}`)
+        dateStringInURL := re.FindString(docBaseUrl)
+        docDate, _      := time.Parse("20060102", dateStringInURL)
+        docContent      := converter.Convert(e.DOM)
+
+        parts := strings.Split(path.Dir(docUrl), "/")
+        savePath := filepath.Join(parts[3],parts[5],parts[6])
+        // fmt.Println("savePath:", savePath)
+        os.MkdirAll(savePath, 0750)
+
+        fileName := colly.SanitizeFileName(docDate.Format("2006-01-02")+"_"+docBaseUrl+".md")
+        // fmt.Println("fileName: ", fileName)
+        if err := os.WriteFile(filepath.Join(savePath, fileName), []byte(docContent), 0666); err != nil {
+            fmt.Println(err)
+        }
+    })
+
+    c.Visit("https://www.vatican.va/content/francesco/fr.html")
+}
